e9fb50dedc2e15878bb4264c4a1a532e5070b7d3
Jason Yang <jasonyang@Jasons-MacBook-Pro-2.local>
2024-02-18 08:10:19 -0800

    Added new motors

--- Wheelchair.ino
+++ Wheelchair.ino
@@ -2,16 +2,20 @@
 #define TRIG_PIN 8
 #define ECHO_PIN 11
 
-const int buttonPin = 7; // Pin number where the button is connected
-int buttonState = 0;     // Variable to hold the state of the button
 Servo servoLeft;
 Servo servoRight;
 int pinServoLeft = 9;
 int pinServoRight = 10;
+Servo servobackLeft;
+Servo servobackRight;
+int pinServobackLeft = 12;
+int pinServobackRight = 13;
 
 void setup() {
   servoLeft.attach(pinServoLeft);
   servoRight.attach(pinServoRight);
+  servobackLeft.attach(pinServobackLeft);
+  servobackRight.attach(pinServobackRight);
   pinMode(TRIG_PIN, OUTPUT); 
   pinMode(ECHO_PIN, INPUT); 
   Serial.begin(9600); // Start serial communication at 9600 baud rate
@@ -20,20 +24,18 @@ void setup() {
 void loop() {
   if (Serial.available() > 0) {
     String command = Serial.readStringUntil('\n'); // Read the incoming command
-    buttonState = digitalRead(buttonPin);
-    if(buttonState == LOW){
-      long duration, distance;
-      digitalWrite(TRIG_PIN, LOW);  
-      delayMicroseconds(2);
-      digitalWrite(TRIG_PIN, HIGH);
-      delayMicroseconds(10);
-      digitalWrite(TRIG_PIN, LOW);
-      duration = pulseIn(ECHO_PIN, HIGH);
-      distance = duration * 0.034 / 2;
-      Serial.print("Distance: ");
-      Serial.println(distance);
-      controlRobot(command, distance); // Control the robot based on the command
-    }
+    long duration, distance;
+    digitalWrite(TRIG_PIN, LOW);  
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+    duration = pulseIn(ECHO_PIN, HIGH);
+    distance = duration * 0.034 / 2;
+    Serial.print("Distance: ");
+    Serial.println(distance);
+    controlRobot(command, distance); // Control the robot based on the command
+    
     delay(1000);
   }
 }
@@ -43,21 +45,31 @@ void controlRobot(String command, long distance) {
     // Forward
     servoLeft.writeMicroseconds(1400); // Adjust as needed for your servo
     servoRight.writeMicroseconds(1600); // Reverse direction for the other servo
+    servobackLeft.writeMicroseconds(1400); // Adjust as needed for your servo
+    servobackRight.writeMicroseconds(1600); // Reverse direction for the other servo
   } else if (command == "down") {
     // Backward
     servoLeft.writeMicroseconds(1600);
     servoRight.writeMicroseconds(1400);
+    servobackLeft.writeMicroseconds(1600);
+    servobackRight.writeMicroseconds(1400);
   } else if (command == "left" && distance > 1) {
     // Turn left
     servoLeft.writeMicroseconds(1600); // Both servos in the same direction
     servoRight.writeMicroseconds(1600);
+    servobackLeft.writeMicroseconds(1600); // Both servos in the same direction
+    servobackRight.writeMicroseconds(1600);
   } else if (command == "right" && distance > 1) {
     // Turn right
     servoLeft.writeMicroseconds(1400); // Both servos in the same direction
     servoRight.writeMicroseconds(1400);
+    servobackLeft.writeMicroseconds(1400); // Both servos in the same direction
+    servobackRight.writeMicroseconds(1400);
   } else {
     // Stop
     servoLeft.writeMicroseconds(1500); // Neutral position - servo stops
     servoRight.writeMicroseconds(1500); // Neutral position - servo stops
+    servobackLeft.writeMicroseconds(1500); // Neutral position - servo stops
+    servobackRight.writeMicroseconds(1500); // Neutral position - servo stops
   }
 }
